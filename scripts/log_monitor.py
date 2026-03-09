861e11d958a48ec812121241489a6cb8d1d7aaec
Srishti Sharma <srishtis2411@gmail.com>
2026-03-09 19:56:14 +0530

    Added alert logging system

--- scripts/log_monitor.py
+++ scripts/log_monitor.py
@@ -1,12 +1,12 @@
 import time
 
 log_file = "logs/system.log"
+alert_file = "alerts/alerts.log"
 
 print("Monitoring logs... Press Ctrl+C to stop.\n")
 
 with open(log_file, "r") as file:
-    
-    # Move to end of file
+
     file.seek(0,2)
 
     while True:
@@ -17,10 +17,18 @@ with open(log_file, "r") as file:
             continue
 
         if "ERROR" in line:
-            print("ALERT: ERROR detected ->", line.strip())
+            alert = "ALERT: ERROR detected -> " + line.strip()
+            print(alert)
+
+            with open(alert_file, "a") as a:
+                a.write(alert + "\n")
 
         elif "WARNING" in line:
-            print("Warning detected ->", line.strip())
+            alert = "Warning detected -> " + line.strip()
+            print(alert)
+
+            with open(alert_file, "a") as a:
+                a.write(alert + "\n")
 
         elif "INFO" in line:
             print("Info ->", line.strip())
